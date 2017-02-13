ad866c6dea8893453d1cb43dd7ee0ff4f48effa2
Srilakshmi GSV <srilakshmi.g@MLABlaptop2.local>
2017-02-13 17:04:34 +0530

    modified addition of cordova plugins till  GPS

--- NewsApp/js/addNews.js
+++ NewsApp/js/addNews.js
@@ -1,3 +1,40 @@
-window.onload = function() {
-	
+function save() {
+$('#save').on('click', function(){
+
+    $('input[type="text"]').each(function(){    
+        var id = $(this).attr('id');
+        var value = $(this).val();
+       localStorage.setItem(id, value);
+
+    });
+  
+    $('input[type="date"]').each(function(){    
+        var id = $(this).attr('id');
+        var value = $(this).val();
+       localStorage.setItem(id, value);
+
+    });  
+    alert("data saved") 
+   
+});
+}
+
+function show() { 
+$('#show').on('click', function(){
+    $('input[type="text"]').each(function(){    
+        var id = $(this).attr('id');
+        var value = localStorage.getItem(id);
+
+        $(this).val(value);
+
+    }); 
+
+    $('input[type="date"]').each(function(){    
+        var id = $(this).attr('id');
+        var value = localStorage.getItem(id);
+
+        $(this).val(value);
+
+    }); 
+});
 }
