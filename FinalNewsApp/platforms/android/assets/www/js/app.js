e0f81176ca4761cab7ce6f696f254856c687d92b
Srilakshmi GSV <srilakshmi.g@MLABlaptop2.local>
2017-02-24 16:16:21 +0530

    finalApplication

---
+++ FinalNewsApp/platforms/android/assets/www/js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('news_app', [
+  'ngRoute', 'ngCordova'
+]);
+
+ app.run(function($window, $rootScope) {
+     $rootScope.online = navigator.onLine;
+     $window.addEventListener("offline", function () {
+         $rootScope.$apply(function() {
+             $rootScope.online = false;
+         });
+     }, false);
+     $window.addEventListener("online", function () {
+         $rootScope.$apply(function() {
+             $rootScope.online = true;
+         });
+     }, false);
+ });
+
+app.config(function($routeProvider) {
+    ($routeProvider)
+      .when('/', {
+          templateUrl : 'pages/login.html',
+          controller : 'loginCtrl'
+      })
+      .when('/category', {
+          templateUrl : 'pages/category.html',
+          controller : 'categoryCtrl'
+      })
+      .when('/newsdisplay/:param', {
+          templateUrl : 'pages/newsDisplay.html',
+          controller : 'categoryCtrl'
+      })
+      .when('/addnews', {
+          templateUrl : 'pages/addNews.html',
+          controller : 'addNewsController'
+      })
+        .when('/showUserNews', {
+            templateUrl : 'pages/showUserNews.html',
+            controller : 'showNewsCtrl'
+        })
+      .otherwise({
+        redirectTo: '/'
+      });
+});
---
+++ FinalNewsApp/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,197 @@
+app.controller('loginCtrl', function($scope, $location,$route) {
+
+    $scope.submitForm = function() {
+        // check to make sure the form is completely valid
+        if ($scope.userForm.$valid) {
+            $location.path('/category');
+        }
+    };
+
+});
+
+app.controller('categoryCtrl', function($scope, $location) {
+    var categoryItem;
+    this.category = categoryList;
+
+    this.getKeys=function () {
+        var keys=[];
+        for (categoryItem in this.category){
+            keys.push(categoryItem);
+        }
+        return keys;
+
+    };
+
+    $scope.newsDisplay = function(categoryItem) {
+        $location.path('/newsdisplay/'+ categoryItem);
+    };
+
+    $scope.goToLogin = function () {
+        $location.path('/');
+    };
+
+    $scope.addNews = function() {
+        $location.path('/addnews');
+    };
+});
+
+app.controller('newsdisplayCtrl', function($scope, $http, $location, $routeParams) {
+    $scope.categoryType = $routeParams;
+    $http({
+       method: 'GET',
+        url: categoryList[$scope.categoryType.param]
+    }).then(function(response) {
+            var xml = response.data;
+            var jsonText = $.xml2json(xml);
+            $scope.newsText = jsonText;
+
+        });
+
+    $scope.goToLogin = function () {
+        $location.path('/');
+    };
+
+    $scope.goTocategory = function() {
+        $location.path('/category');
+    }
+});
+
+app.controller('addNewsController',
+    function($scope, $location, $cordovaCamera, $http, $cordovaCapture, $cordovaGeolocation) {
+        localStorage.setItem('newsItem', JSON.stringify($scope.newsItem));
+
+        $scope.addNews = function() {
+            $scope.newsItem = [];
+            $scope.newsItem.push({
+
+                headline: $scope.headline,
+                date: $scope.date,
+                description:$scope.newsText,
+                reporter: $scope.reporter,
+                image : $scope.imgURI,
+                location:$scope.address,
+                video:$scope.pathVideo,
+                done: false
+            });
+
+            $scope.headline = '';
+            $scope.date = '';
+            $scope.newsText = '';
+            $scope.reporter = '';
+            localStorage.setItem('newsItem', JSON.stringify($scope.newsItem));
+
+            $scope.geolocation();
+            $scope.showUserNews();
+        };
+
+            $scope.takePhoto = function () {
+                var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.CAMERA,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+
+                $cordovaCamera.getPicture(options).then(function (imageData) {
+                    $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                }, function (err) {
+                    // An error occured.
+                });
+            };
+
+            $scope.choosePhoto = function () {
+                var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+
+                $cordovaCamera.getPicture(options).then(function (imageData) {
+                    $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                }, function (err) {
+                    // An error occured. Show a message to the user
+                });
+            };
+
+        $scope.geolocation = function() {
+            if(navigator.geolocation)
+                navigator.geolocation.getCurrentPosition(onPositionUpdate);
+
+
+        function onPositionUpdate(position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true";
+
+            $http.get(url).then(function(result) {
+                var address = result.data.results[2].formatted_address;
+                $scope.address = address;
+                alert($scope.address);
+            });
+        }};
+
+        $scope.captureAudio = function() {
+                var options = { limit: 2, duration: 20 };
+
+                $cordovaCapture.captureAudio(options).then(function(audioData) {
+                  // Success! Audio data is here
+                  var i, pathAudio, len;
+
+                        for (i = 0, len = audioData.length; i < len; i += 1) {
+                           pathAudio = audioData[i].fullPath;
+                           console.log(audioData);
+                        }
+                }, function(err) {
+                  // An error occurred. Show a message to the user
+                });
+              }
+
+              $scope.captureVideo = function() {
+                var options = { limit: 2, duration: 15 };
+
+                $cordovaCapture.captureVideo(options).then(function(videoData) {
+                  // Success! Video data is here
+                    var i, pathVideo, len;
+
+                        for (i = 0, len = videoData.length; i < len; i += 1) {
+                           pathVideo = videoData[i].fullPath;
+                           console.log(videoData);
+                        }
+                }, function(err) {
+                  // An error occurred. Show a message to the user
+                });
+              }
+
+        $scope.category = function() {
+            $location.path('/category');
+        };
+
+        $scope.showUserNews = function() {
+            $location.path('/showUserNews');
+        };
+
+    }
+);
+
+app.controller('showNewsCtrl', function($scope, $location){
+     $scope.news_article = JSON.parse(localStorage.getItem('newsItem'));
+
+     $scope.goAddNews = function() {
+        $location.path('/addnews');
+     }
+
+     $scope.logout = function() {
+        $location.path('/')
+     }
+});
